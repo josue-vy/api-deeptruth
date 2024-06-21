3709210a3714130c651f00c2f7b60c3ee9fade5b
josue-vy <josvillanuevay@upt.pe>
2024-06-20 23:45:17 -0500

    api-deeptruht v0.1.0

---
+++ src/app.py
@@ -0,0 +1,17 @@
+
+from fastapi import FastAPI
+from routers import faceRouter
+from starlette.middleware import Middleware
+import uvicorn
+from middlewares import error
+from services.faceService import deepFaceServiceInstance
+from datetime import datetime
+
+app = FastAPI()
+app.add_middleware(error.ErrorHandlingMiddleware)
+app.include_router(faceRouter, prefix="/api", tags=["api"])
+@app.get("/")
+async def read_root():
+    current_datetime = datetime.now()
+    formatted_datetime = current_datetime.strftime("%Y-%m-%d %H:%M:%S")
+    return {"message": "Current Now API DISCO : "+str(formatted_datetime)}
